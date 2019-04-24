6a179eef97c19ef789b500b50e8f2748c8cff573
mkaraki <backtrack1994+github@gmail.com>
2019-04-24 22:38:33 +0900

    Change Something

--- mkdiscordbot/Program.cs
+++ mkdiscordbot/Program.cs
@@ -243,12 +243,12 @@ namespace mkdiscordbot
         }
 
 #pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
 
-        private async Task MessageReceivedAsync(SocketMessage message)
-        {
-            _ = Task.Run(() => Cmds.CmdHandle(message));
-        }
+        private async Task MessageReceivedAsync(SocketMessage message) 
+            => Task.Run(() => Cmds.CmdHandle(message));
 
 #pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
     }
 }
